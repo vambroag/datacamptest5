cf268c92230962cc8c49f2101ac99e59de48f441
vambroag <vambroag@gmail.com>
2023-08-28 10:56:32 +0900

    08/28

--- survey/connect.php
+++ survey/connect.php
@@ -1,10 +1,10 @@
 <?php
-$server = "1.234.240.70:3306";
+$server = "localhost";
 $user = "root";
-$password = "";
+$password = "1234";
 $dbname = "datacamp";
 
-$conn = new mysqli($server, $user, $password, $dbname);
+$conn = mysqli_connect($server, $user, $password, $dbname);
 
 if($conn -> connect_error) echo "<h2>접속에 실패하였습니다</h2>";
 else echo "<h2>접속에 성공하였습니다</h2>";
